383276f431b622be93bd27d2aa780c93f2c25b46
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-16 14:18:58 +0100

    Mi aggiungo delle funzioni utili per il programma

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ Dopo che sono stati inseriti i 5 numeri, il software
 dice quanti e quali dei numeri da indovinare sono
 stati individuati
 */
+
+//FUNZIONI utili per il programma
+
+//Funzione per generare numero random
+function randomNumber( min, max) {
+ return parseInt(Math.random() * (max - min) + min + 1) ;
+}
+//Funzione per cercare un elemento in un array
+function isInArray(array, item) {
+  var i = 0;
+  var risultato = false;
+  while (i < array.length && risultato == false) {
+    if (array[i] == item) {
+      risultato = true;
+    }
+    i++;
+  }
+  return risultato;
+}
