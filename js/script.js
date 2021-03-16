e71e494f13e81d3135e34f0fdef99a24f474e782
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-16 15:27:37 +0100

    stampo il risultato

--- js/script.js
+++ js/script.js
@@ -26,18 +26,31 @@ function isInArray(array, item) {
   }
   return risultato;
 }
-
+//Variabili utili
 var winningNumbers = [];
+var numbersPrompt = [];
 //Creo il ciclo for per generare 5 numeri random e li metto nell'array vuoto
 for (var i = 0; i < 5; i++) {
   winningNumbers.push(randomNumber(1,60));
 }
 //Stampo i numeri con l'alert
-var startGame = alert('I numeri vincenti sono : ' + winningNumbers);
+var startGame = alert('Il browser ha generato 5 numeri random vincenti tra 1 e 60, i numeri sono : ' + winningNumbers);
+console.log(winningNumbers);
 
 //Inizializzo il setTimeout
 setTimeout(function () {
+  //Chiedo all'utente 5 volte di inserire un numero
   for (var j = 0; j < 5; j++) {
     var gamePrompts = parseInt(prompt('Inserisci uno dei numeri che hai visto prima'));
-  }
-}, 2000);
+  //Inizializzo un altro ciclo for per definire i numeri che inserisce l'utente
+    for (var y = 0; y < winningNumbers.length; y++) {
+      //Verifico che siano uguali ai numeri inseriti
+      if ( gamePrompts == winningNumbers[y] ) {
+       numbersPrompt.push(gamePrompts);
+       console.log(winningNumbers[y]);
+      }
+    }
+
+  }//Mostro il risultato con un alert
+  alert('hai ricordato i numeri ' + numbersPrompt + ' il tuo punteggio è: ' + numbersPrompt.length );
+}, 3000);
